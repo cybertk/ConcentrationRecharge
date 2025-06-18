29738039925a5ae8c75af3d2fcf1175dc138a080
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-06-18 16:56:29 +0300

    Create ruRU.lua

---
+++ Locales/ruRU.lua
@@ -0,0 +1,8 @@
+local _, ns = ...
+
+local L = {}
+ns.L = L
+--Translator ZamestoTV
+L["show_all_characters"] = "<Нажмите CTRL, чтобы показать всех персонажей>"
+L["show_glow_effect_on_spell_icon"] = "Показывать эффект свечения на иконке заклинания |cffffffff%s|r, когда концентрация полностью восстановлена"
+L["enable_glow_effects"] = "Включить эффекты свечения перезарядки"
